9b041c7f1598d568c10b6b9d2c42c9f82fcce878
AchiniSamarasinghe <70306566+AchiniSamarasinghe@users.noreply.github.com>
2020-09-28 10:55:57 +0530

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript">
+	function validate(){
+		
+		var email = document.forms['frm']['email'];
+		
+		
+		if (email.value == "") {
+			window.alert("Please enter your E-mail."); 
+       		email.focus();  
+			return false;
+		}
+		if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)) {
+			return true;
+		}else{
+			window.alert("Enter valid email"); 
+            email.focus(); 
+            return false;
+		}
+		
+
+		return false;
+	}
+</script>
+
+</body>
+</html>
